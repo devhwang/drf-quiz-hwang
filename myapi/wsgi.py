838b45ff598500ac69a706ce41f7e6df33b067a1
Kiha <kiha@Kihaui-MacBookAir.local>
2021-02-12 19:48:24 +0900

    commit

--- myapi/wsgi.py
+++ myapi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf-quiz-hwang.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapi.settings')
 
 application = get_wsgi_application()
